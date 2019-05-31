ae387c48e1491f3824556938fe26d9c053082082
williamsoftwarecode <williamsoftwarecode@gmail.com>
2019-05-31 20:46:29 +0800

    Lifecycle Hooks

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  Input, 
+  ViewEncapsulation,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked, 
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy  
+  } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,12 +19,42 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated //None and Native
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements 
+OnInit, 
+OnChanges, 
+DoCheck, 
+AfterContentInit, 
+AfterContentChecked,
+AfterViewInit, 
+AfterViewChecked, 
+OnDestroy {
   @Input('srvElement') element: {type: string, name: string, content: string};
+  @ViewChild('heading') header: ElementRef;
+  @ContentChild('contentParagraph') paragraph: ElementRef;
 
   constructor() { }
+
+  ngOnChanges(changes: SimpleChanges) {
+  }
 
   ngOnInit() {
   }
 
+  ngDoCheck() {
+  }
+
+  ngAfterContentInit() {
+  }
+
+  ngAfterContentChecked() {
+  }
+
+  ngAfterViewInit() {
+  }
+
+  ngAfterViewChecked() {
+  }
+
+  onDestroy() {
+  }
 }
